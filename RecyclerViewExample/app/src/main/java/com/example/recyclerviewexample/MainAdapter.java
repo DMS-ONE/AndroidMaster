f3d46a79e923835239590e0f8775e66148f697ea
parkUiery <parkuiery@gmail.com>
2023-05-23 17:16:23 +0900

    refactor :: viewbinding 코드 작성

--- RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/MainAdapter.java
+++ RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/MainAdapter.java
@@ -2,6 +2,7 @@ package com.example.recyclerviewexample;
 
 
 import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,51 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
 
     private ArrayList<String> locallDataSet;
 
+<<<<<<< Updated upstream
+=======
+    private List<ClipData.Item> itemList;
+
+    public MainAdapter(List<ClipData.Item> itemList){
+        this.itemList = itemList;
+    }
+
+    //뷰 홀더 클래스
+   public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView textView;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+
+            itemView.setOnClickListener((View.OnClickListener) this);
+        }
+        public TextView getTextView(){
+            return textView;
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            if(position != RecyclerView.NO_POSITION) {
+                swapItems(position);
+            }
+        }
+
+        private void swapItems(int position) {
+            int newPosition = 1;
+
+            Collections.swap(itemList, position, newPosition);
+            notifyItemMoved(position, newPosition);
+        }
+
+    }
+>>>>>>> Stashed changes
 
     //생성자
     public MainAdapter(ArrayList<String> dataSet) {
--- RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/SecoundActivity.java
+++ RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/SecoundActivity.java
@@ -10,7 +10,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.recyclerviewexample.databinding.ActivitySecoundActivityBinding;
+
 public class SecoundActivity extends AppCompatActivity {
+
+    private ActivitySecoundActivityBinding binding;
 
     private Button showButton;
     private EditText inputEditText;
@@ -20,15 +24,45 @@ public class SecoundActivity extends AppCompatActivity {
         super.onCreate(savedInstaanceState);
         setContentView(R.layout.activity_secound_activity);
 
-        TextView itemNumberTextView = findViewById(R.id.itemNumberTextView);
+        binding =  ActivitySecoundActivityBinding.inflate(getLayoutInflater());
 
+        View view = binding.getRoot();
+        setContentView(view);
+
+<<<<<<< Updated upstream
         Intent intent = getIntent();
         int itemNumber = intent.getIntExtra("item_number", -1);
+=======
+<<<<<<< Updated upstream
+        Intent intent =getIntent();
+        int itemNumber = intent.getIntExtra("item_number",-1);
+=======
+        //TextView itemNumberTextView = findViewById(R.id.itemNumberTextView);
+        TextView itemNumberTextView = binding.itemNumberTextView;
+
+<<<<<<< Updated upstream
+        Intent intent = getIntent();
+        int itemNumber = intent.getIntExtra("item_number", -1);
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
         itemNumberTextView.setText("Item Number: " + itemNumber);
 
         showButton = findViewById(R.id.showButton);
         inputEditText = findViewById(R.id.inputEditText);
+=======
+        Intent intent =getIntent();
+        int itemNumber = intent.getIntExtra("item_n.umber",-1);
+
+        itemNumberTextView.setText("Item Number: " + itemNumber);
+
+
+
+        //showButton = findViewById(R.id.showButton);
+        Button showButton = binding.showButton;
+        //inputEditText = findViewById(R.id.inputEditText);
+        EditText inputEditText = binding.inputEditText;
+>>>>>>> Stashed changes
 
         showButton.setOnClickListener(new View.OnClickListener() {
 
